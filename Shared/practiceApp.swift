bc921f62bd5e7e66dc778380b65f4680fc6b1906
Yuni-Q <lyh6425@gmail.com>
2021-03-29 12:46:45 +0900

    Initial Commit

---
+++ Shared/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  Shared
+//
+//  Created by yuni on 2021/03/29.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
